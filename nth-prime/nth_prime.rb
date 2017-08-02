479317d7d54f707d5c804467c0bdaa5f9c2aefb1
omaotzu <omarhphillips@gmail.com>
2017-08-02 11:12:19 +0100

    2nd iteration

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -1,13 +1,20 @@
 module BookKeeping
   VERSION = 1
 end
-require 'prime'
 
 class Prime
-  def self.nth(number)
-    raise ArgumentError if number <1
-  else
-    results = Prime.take(number)
-    return results.last
+  @primes = []
+  def self.nth(input)
+    number = 2
+    while @primes.size < input do
+      @primes << number if is_prime?(number)
+      number += 1
+    end
+    @primes[input-1]
+  end
+
+  def self.is_prime?(input)
+    @primes.each { |prime| return false if input % prime == 0 }
+    true
   end
 end
